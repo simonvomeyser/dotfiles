fa63145d6c6bc99865f0e42cf9146de349a99070
Simon vom Eyser <simon.vom.eyser@gmail.com>
2023-02-02 10:47:38 +0100

    feat: git keymaps up to date with tut

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -1,45 +1,44 @@
+-- Space is my leader.
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
-vim.keymap.set('n', '<leader>k', ':nohlsearch<CR>')
-vim.keymap.set('n', '<leader>Q', ':bufdo bdelete<CR>')
+-- When text is wrapped, move by terminal rows, not lines, unless a count is provided.
+vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true })
+vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
 
--- Allow gf to open non-existent files
-vim.keymap.set('', 'gf', ':edit <cfile><CR>')
-
--- Reselect visual selection after indenting
+-- Reselect visual selection after indenting.
 vim.keymap.set('v', '<', '<gv')
 vim.keymap.set('v', '>', '>gv')
 
--- Maintain the cursor position when yanking a visual selection
+-- Maintain the cursor position when yanking a visual selection.
 -- http://ddrscott.github.io/blog/2016/yank-without-jank/
 vim.keymap.set('v', 'y', 'myy`y')
-vim.keymap.set('v', 'Y', 'myY`y')
 
--- When text is wrapped, move by terminal rows, not lines, unless a count is provided
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true })
+-- Disable annoying command line typo.
+vim.keymap.set('n', 'q:', ':q')
 
--- Paste replace visual selection without copying it
+-- Paste replace visual selection without copying it.
 vim.keymap.set('v', 'p', '"_dP')
 
--- Easy insertion of a trailing ; or , from insert mode
-vim.keymap.set('i', ';;', '<Esc>A;<Esc>')
-vim.keymap.set('i', ',,', '<Esc>A,<Esc>')
+-- Easy insertion of a trailing ; or , from insert mode.
+vim.keymap.set('i', ';;', '<Esc>A;')
+vim.keymap.set('i', ',,', '<Esc>A,')
 
--- Open the current file in the default program (on Mac this should just be just `open`)
-vim.keymap.set('n', '<leader>x', ':!xdg-open %<cr><cr>')
+-- Quickly clear search highlighting.
+vim.keymap.set('n', '<Leader>k', ':nohlsearch<CR>')
 
--- Disable annoying command line thing
-vim.keymap.set('n', 'q:', ':q<CR>')
+-- Open the current file in the default program (on Mac this should just be just `open`).
+vim.keymap.set('n', '<Leader>x', ':!open %<CR><CR>')
 
--- Resize with arrows
-vim.keymap.set('n', '<C-Up>', ':resize +2<CR>')
-vim.keymap.set('n', '<C-Down>', ':resize -2<CR>')
-vim.keymap.set('n', '<C-Left>', ':vertical resize -2<CR>')
-vim.keymap.set('n', '<C-Right>', ':vertical resize +2<CR>')
-
--- Move text up and down
+-- Move lines up and down.
 vim.keymap.set('i', '<A-j>', '<Esc>:move .+1<CR>==gi')
 vim.keymap.set('i', '<A-k>', '<Esc>:move .-2<CR>==gi')
-vim.keymap.set('x', '<A-j>', ":move '>+1<CR>gv-gv")
-vim.keymap.set('x', '<A-k>', ":move '<-2<CR>gv-gv")
+vim.keymap.set('n', '<A-j>', ':move .+1<CR>==')
+vim.keymap.set('n', '<A-k>', ':move .-2<CR>==')
+vim.keymap.set('v', '<A-j>', ":move '>+1<CR>gv=gv")
+vim.keymap.set('v', '<A-k>', ":move '<-2<CR>gv=gv")
+
+-- Quickly save
+vim.keymap.set('n', '<Leader>w', ":w<Enter>")
+
+
