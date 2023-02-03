111ca3a2d9488eb8eeddfaf1cb61fcf168f60153
Simon vom Eyser <simon.vom.eyser@gmail.com>
2023-02-03 11:07:43 +0100

    wip

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -148,13 +148,13 @@ use({
 })
 
 -- File tree sidebar
--- use({
---   'kyazdani42/nvim-tree.lua',
---   requires = 'kyazdani42/nvim-web-devicons',
---   config = function()
---     require('user/plugins/nvim-tree')
---   end,
--- })
+use({
+  'kyazdani42/nvim-tree.lua',
+  requires = 'kyazdani42/nvim-web-devicons',
+  config = function()
+    require('user/plugins/nvim-tree')
+  end,
+})
 
 -- Automatically set up your configuration after cloning packer.nvim
 -- Put this at the end after all plugins
