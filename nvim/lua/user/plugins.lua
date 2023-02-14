d22e5237ae3d414c59cc6aeec142a6ab56bc3393
Simon vom Eyser <simon.vom.eyser@gmail.com>
2023-02-14 16:38:38 +0100

    feat: add new git keymaps

--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -211,11 +211,13 @@ use({
   'lewis6991/gitsigns.nvim',
   config = function()
     require('gitsigns').setup()
-    vim.keymap.set('n', ']h', ':Gitsigns next_hunk<CR>')
-    vim.keymap.set('n', '[h', ':Gitsigns prev_hunk<CR>')
+    vim.keymap.set('n', 'gn', ':Gitsigns next_hunk<CR>')
+    vim.keymap.set('n', 'gp', ':Gitsigns prev_hunk<CR>')
     vim.keymap.set('n', 'gs', ':Gitsigns stage_hunk<CR>')
     vim.keymap.set('n', 'gS', ':Gitsigns undo_stage_hunk<CR>')
-    vim.keymap.set('n', 'gp', ':Gitsigns preview_hunk<CR>')
+    vim.keymap.set('n', 'gr', ':Gitsigns reset_hunk<CR>')
+    vim.keymap.set('n', 'gR', ':Gitsigns reset_buffer<CR>')
+    vim.keymap.set('n', 'gP', ':Gitsigns preview_hunk<CR>')
     vim.keymap.set('n', 'gb', ':Gitsigns blame_line<CR>')
   end,
 })
