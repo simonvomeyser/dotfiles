81a0aaac4ea15beb5c116ff90785e781766eb4df
Simon vom Eyser <simon.vom.eyser@gmail.com>
2023-02-17 07:03:55 +0100

    feat: add auto completion

--- nvim/lua/user/options.lua
+++ nvim/lua/user/options.lua
@@ -32,3 +32,4 @@ vim.opt.updatetime = 4001 -- Set updatime to 1ms longer than the default to prev
 vim.opt.redrawtime = 10000 -- Allow more time for loading syntax on large files
 
 vim.opt.signcolumn = 'yes:2'
+vim.opt.completeopt = 'menuone,longest,preview'
